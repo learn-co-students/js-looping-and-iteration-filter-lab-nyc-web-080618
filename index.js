5bdbea973ff9809702d3bda72e2ca9888ee0104a
wangyan6783 <wangyan6783@gmail.com>
2018-09-16 22:23:19 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// Code your solution in this file
+function findMatching(drivers, name) {
+  return drivers.filter(function(n) {
+    return n.toUpperCase() === name.toUpperCase();
+  });
+}
+
+function fuzzyMatch(drivers, letters){
+  return drivers.filter(function(name) {
+    return name.startsWith(letters);
+  });
+}
+
+function matchName(drivers, name){
+  return drivers.filter(function(driver){
+    return driver.name === name;
+  });
+}
